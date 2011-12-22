05a97680c47eadad7594181de26d177699f87f2f
Peter O'Shaughnessy <p_os@yahoo.co.uk>
2011-12-22 17:42:41 +0000

    Moved everything back from the public directory

--- app.js
+++ app.js
@@ -3,11 +3,15 @@ var express = require('express');
 var app = express.createServer();
 
 app.get('/', function(request, response) {
-    response.sendfile(__dirname + '/public/index.html');
+    response.sendfile(__dirname + '/index.html');
 });
 
 app.configure(function() {
-    app.use('/public', express.static(__dirname + '/public'));
+    app.use('/css', express.static(__dirname + '/css'));
+    app.use('/js', express.static(__dirname + '/js'));
+    app.use('/images', express.static(__dirname + '/images'));
+    app.use('/2010', express.static(__dirname + '/2010'));
+    app.use('/2011', express.static(__dirname + '/2011'));
 });
 
 var port = process.env.PORT || 3000;
