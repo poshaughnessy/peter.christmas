e35533c65865366db6243247feab752eca8f4777
Peter O'Shaughnessy <peter.oshaughnessy@pearson.com>
2013-12-21 19:42:49 +0000

    Fix broken app

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express.createServer();
 
 app.get('/', function(request, response) {
-    response.sendfile(__dirname + '/index.html');
+    response.sendfile('/index.html', {root: __dirname});
 });
 
 app.configure(function() {
@@ -13,6 +13,7 @@ app.configure(function() {
     app.use('/2010', express.static(__dirname + '/2010'));
     app.use('/2011', express.static(__dirname + '/2011'));
     app.use('/2012', express.static(__dirname + '/2012'));
+    app.use('/2013', express.static(__dirname + '/2013'));
 });
 
 var port = process.env.PORT || 3000;
